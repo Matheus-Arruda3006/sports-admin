a78a7af2cec3d3170f87133d33718fe9d99a3957
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2023-12-31 18:29:28 -0300

    dashboard setup

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -18,10 +18,5 @@ export default function Home() {
     },[isOpen, onOpen]);
 
 
-    return (
-      <div className="p-4">
-         <UserButton/>
-         Root page
-      </div>
-    )
+    return null;
   }
